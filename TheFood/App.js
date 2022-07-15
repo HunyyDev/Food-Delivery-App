4d4abda9e594aeff848c49495a9f4832681f8cf9
tanluuuuuuu <20521591@gm.uit.edu.vn>
2022-07-15 20:24:53 +0700

    clean

--- TheFood/App.js
+++ TheFood/App.js
@@ -30,26 +30,6 @@ const Section = ({children, title}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-      <Text>Just for fun</Text>
-      <View>View</View>
     </View>
   );
 };
