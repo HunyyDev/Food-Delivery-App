06713fd391cf7e376e670353f098f3f918e6981d
Huỳnh Viết Tuấn Kiệt <hvtkiet.15092002@gmail.com>
2022-07-18 20:55:39 +0700

    addView

--- TheFood/App.js
+++ TheFood/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import OnBoardingScreen from './src/screens/onboarding';
+import {View} from 'react-native';
 
 const App = () => {
   return (
-    <>
+    <View>
       <OnBoardingScreen />
-    </>
+    </View>
   );
 };
 
