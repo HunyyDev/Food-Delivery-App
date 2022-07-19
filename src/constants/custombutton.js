5bc776e004b66d43995662f1d4e86f5158fc4d09
LAPTOP-S74F9L51\Ideapad gaming 3 <univerbachtuoc@gmail.com>
2022-07-19 14:53:46 +0700

    update: CustomButton

--- src/constants/custombutton.js
+++ src/constants/custombutton.js
@@ -2,34 +2,39 @@ import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import React from 'react';
 import FONT_FAMILY from './fonts';
 import CUSTOM_COLOR from './colors';
+import {Component} from 'react';
 
-const CustomButton = props => {
-  console.log('props', props);
-  if (props.type === 'primary') {
-    return (
-      <TouchableOpacity>
-        <View
-          style={styles.ButtonContainer}
-          backgroundColor={CUSTOM_COLOR.White}>
-          <Text style={(styles.Text, {color: CUSTOM_COLOR.Vermilion})}>
-            Get started
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
+class CustomButton extends Component {
+  constructor(props) {
+    super(props);
   }
-  if (props.type === 'secondary') {
-    return (
-      <TouchableOpacity>
-        <View
-          style={styles.ButtonContainer}
-          backgroundColor={CUSTOM_COLOR.Vermilion}>
-          <Text style={(styles.Text, {color: CUSTOM_COLOR.White})}>Login</Text>
-        </View>
-      </TouchableOpacity>
-    );
+  render() {
+    if (this.props.type === 'primary') {
+      return (
+        <TouchableOpacity>
+          <View
+            style={styles.ButtonContainer}
+            backgroundColor={CUSTOM_COLOR.White}>
+            <Text style={(styles.Text, {color: CUSTOM_COLOR.Vermilion})}>
+              Get started
+            </Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+    if (this.props.type === 'secondary') {
+      return (
+        <TouchableOpacity>
+          <View
+            style={styles.ButtonContainer}
+            backgroundColor={CUSTOM_COLOR.Vermilion}>
+            <Text style={(styles.Text, {color: CUSTOM_COLOR.White})}>Login</Text>
+          </View>
+        </TouchableOpacity>
+      );
+    }
   }
-};
+}
 
 export default CustomButton;
 
