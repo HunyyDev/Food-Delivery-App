5ca3ba8129b2fe595f11ea54b11dfc40ae31a15c
nn_truong1106 <truong11062002@gmail.com>
2022-07-20 09:09:46 +0700

    update login screen and gitignore

--- FoodDeliveryApp/src/screens/auth/loginScreen.js
+++ FoodDeliveryApp/src/screens/auth/loginScreen.js
@@ -1,78 +1,95 @@
 import {
   Text,
-  View,
   StyleSheet,
-  SafeAreaView,
+  View,
   Image,
   TouchableOpacity,
-  TextInput,
+  SafeAreaView,
 } from 'react-native';
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import CUSTOM_COLOR from '../../constants/colors';
-import FONT_FAMILY from '../../constants/fonts';
 import {IMG_Logo} from '../../assets/images';
-import CustomButton from '../../components/CustomButton';
 import scale from '../../constants/responsive';
+import CustomInput from '../../components/CustomInput';
+import FONT_FAMILY from '../../constants/fonts';
+import CustomButton from '../../components/CustomButton';
 
-const CustomInput = props => {
-  // console.log('props', props);
-  // console.log('secureText', props.secureText);
-  return (
-    <>
-      <Text style={styles.textInput}>{props.label}</Text>
-      <TextInput
-        secureTextEntry={props.secureText}
-        placeholder={props.label}
-        style={styles.inputContainer}
-      />
-    </>
-  );
-};
+const LOGIN = 'LOGIN';
+const SIGN_UP = 'SIGN_UP';
 
-export class LoginScreen extends Component {
+export default class LoginScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: LOGIN,
+    };
+  }
+
+  setPage = newPage => {
+    this.setState({page: newPage});
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        {/* sectionHeading */}
-        <View style={styles.sectionHeading}>
-          <View style={styles.imageHeading}>
-            <Image source={IMG_Logo} style={styles.loginLOGO} />
+      <>
+        <SafeAreaView style={styles.container}>
+          {/* headerSection */}
+          <View style={styles.headerSection}>
+            <Image
+              source={IMG_Logo}
+              style={styles.logo}
+              resizeMode={'contain'}
+            />
+
+            <View style={styles.headerText}>
+              <TouchableOpacity
+                style={styles.loginAndSignUp}
+                onPress={() => {
+                  this.setPage(LOGIN);
+                }}>
+                <Text style={styles.text}>Login</Text>
+                {this.state.page === LOGIN ? (
+                  <View style={styles.line}></View>
+                ) : null}
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.loginAndSignUp}
+                onPress={() => {
+                  this.setPage(SIGN_UP);
+                }}>
+                <Text style={styles.text}>Sign-up</Text>
+                {this.state.page === SIGN_UP ? (
+                  <View style={styles.line}></View>
+                ) : null}
+              </TouchableOpacity>
+            </View>
           </View>
 
-          <View style={styles.textHeading}>
-            <TouchableOpacity>
-              <Text style={styles.textLogin}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Text style={styles.textSignUp}>Sign-up</Text>
-            </TouchableOpacity>
+          {/* inputSection */}
+          <View style={styles.inputSection}>
+            <View>
+              {/* Email address */}
+              <CustomInput label={'Email address'} />
+
+              {/* Password */}
+              <CustomInput label={'Password'} secureText={true} />
+
+              <TouchableOpacity>
+                <Text style={styles.forgotPasscode}>Forgot passcode?</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-        {/* sectionInput */}
 
-        <View style={styles.sectionInput}>
-          <View>
-            {/* Email address */}
-            <CustomInput label={'Email address'} />
-
-            {/* Password */}
-            <CustomInput label={'Password'} secureText={true} />
-
-            <TouchableOpacity>
-              <Text style={styles.forgotPasscode}>Forgot passcode?</Text>
-            </TouchableOpacity>
+          {/* buttonSection */}
+          <View style={styles.buttonSection}>
+            <CustomButton type={'secondary'} text={'Login'} />
           </View>
-        </View>
-        {/* sectionButton */}
-        <View style={styles.sectionButton}>
-          <CustomButton text={'Login'} />
-        </View>
-      </SafeAreaView>
+        </SafeAreaView>
+      </>
     );
   }
 }
-
-export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -80,70 +97,63 @@ const styles = StyleSheet.create({
     backgroundColor: CUSTOM_COLOR.Silver,
   },
 
-  sectionHeading: {
-    flex: 4,
+  headerSection: {
+    flex: 5,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    borderRadius: 30,
     backgroundColor: CUSTOM_COLOR.White,
-    borderBottomLeftRadius: 30,
-    borderBottomRightRadius: 30,
   },
-  imageHeading: {
-    flex: 3,
+
+  headerText: {
+    width: '100%',
+    height: '20%',
+    marginTop: 15,
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+  },
+
+  text: {
+    fontSize: scale(18),
+    fontFamily: FONT_FAMILY.ExtraBold,
+  },
+
+  logo: {
+    height: scale(162.38),
+    width: scale(131.53),
+  },
+
+  inputSection: {
+    flex: 6,
+    backgroundColor: CUSTOM_COLOR.Silver,
+    margin: 30,
+  },
+
+  buttonSection: {
+    flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  loginLOGO: {
-    width: scale(131.53),
-    height: scale(162.38),
+    bottom: 20,
   },
 
-  textHeading: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    marginBottom: 10,
+  line: {
+    height: 3,
+    width: '65%',
+    backgroundColor: CUSTOM_COLOR.Vermilion,
+    position: 'absolute',
+    bottom: 0,
   },
-  textLogin: {
-    fontFamily: FONT_FAMILY.ExtraBold,
-    fontSize: scale(17),
-    color: CUSTOM_COLOR.Black,
-  },
-
-  textSignUp: {
-    fontFamily: FONT_FAMILY.ExtraBold,
-    fontSize: scale(17),
-    color: CUSTOM_COLOR.Black,
+  loginAndSignUp: {
+    width: '50%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
-  sectionInput: {
-    flex: 4,
-    marginTop: 25,
-    marginHorizontal: 30,
-  },
-
-  inputContainer: {
-    backgroundColor: CUSTOM_COLOR.Silver,
-    marginTop: 10,
-    borderButtonColor: CUSTOM_COLOR.Black,
-    borderRightColor: CUSTOM_COLOR.Silver,
-    borderLeftColor: CUSTOM_COLOR.Silver,
-    borderTopColor: CUSTOM_COLOR.Silver,
-    borderWidth: 1,
-    fontFamily: FONT_FAMILY.ExtraBold,
-  },
-
-  textInput: {
-    marginTop: 20,
-    fontFamily: FONT_FAMILY.ExtraBold,
-    width: scale(200),
-    height: scale(20),
-  },
   forgotPasscode: {
     marginTop: 20,
     color: CUSTOM_COLOR.SunsetOrange,
     fontFamily: FONT_FAMILY.Bold,
     fontSize: scale(17),
-  },
-  sectionButton: {
-    flex: 1,
-    backgroundColor: CUSTOM_COLOR.Silver,
   },
 });
