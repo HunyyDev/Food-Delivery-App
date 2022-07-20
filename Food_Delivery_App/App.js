60501f9d5236fd0bc154e5b34e7ebbb41a82a8de
DESKTOP-RHGLNSB\UIT <21522246@gm.uit.edu.vn>
2022-07-20 23:21:06 +0700

    dev: CustomButton

--- Food_Delivery_App/App.js
+++ Food_Delivery_App/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import OnBoardingScreen from './src/screens/OnBoardingScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import CustomButton from './src/components/CustomButton';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <>
-      <OnBoardingScreen />
-    </>
+    <SafeAreaView style={styles.container}>
+      <CustomButton
+        title={'Login'}
+        alert={"Login button's event handle"}
+        type={'secondary'} //type=[‘primary’, ‘secondary’
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ccc',
+    // alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
 
 export default App;
---
+++ Food_Delivery_App/src/components/CustomButton/index.js
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+import {StyleSheet, Text, Alert} from 'react-native';
+import React from 'react';
+import {scaleX, scaleY} from '../../helperFunction.js';
+
+const CustomButton = props => {
+  const [buttonColor, setbuttonColor] = useState(() => {
+    if (props.type === 'primary') return {text: '#FF460A', background: '#fff'};
+    else if (props.type === 'secondary')
+      return {text: '#F6F6F9', background: '#FA4A0C'};
+    else return {text: '#fff', background: '#000'};
+  });
+
+  const PressHandler = () => {
+    Alert.alert(props.title, props.alert, [
+      {
+        text: 'CLOSE',
+      },
+      {
+        text: 'CLOSE too',
+      },
+    ]);
+  };
+  return (
+    <Text
+      style={[
+        styles.buttonStyle.text,
+        styles.buttonStyle,
+        {backgroundColor: buttonColor.background, color: buttonColor.text},
+      ]}
+      onPress={PressHandler}>
+      {props.title}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    padding: 25,
+    // backgroundColor: buttonColor.background,
+    borderRadius: 70,
+    textAlign: 'center',
+
+    text: {
+      // color: buttonColor.text,
+      fontSize: 17,
+    },
+
+    marginHorizontal: scaleY(1),
+    position: 'absolute',
+    left: 50,
+    right: 50,
+    bottom: 41,
+  },
+});
+
+export default CustomButton;
