336fb643dd5d1ccc989b2d2ac9e231a08f73fa9c
Tinnn <ngoctin0809@gmail.com>
2022-07-21 19:01:43 +0700

    Update: Try to fix branch

---
+++ Tournesol/src/constants/colors.js
@@ -0,0 +1,10 @@
+const CUSTOM_COLOR = {
+    SilverSand : '#C5C4C4',
+    SunsetOrange: '#FA4A0C',
+    White: '#FFF',
+    Black: '#000',
+    Search: '#EFEEEE',
+    Font_Navigation: '#9A9A9D',
+};
+
+export default CUSTOM_COLOR;
---
+++ Tournesol/src/constants/reponsive.js
@@ -0,0 +1,7 @@
+import { Dimensions } from "react-native";
+const Size = {
+    Device_Width: Dimensions.get('window').width,
+    Device_Height: Dimensions.get('window').height,
+  };
+  export default Size;
+  
