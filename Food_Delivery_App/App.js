39a873af36d4cff10ee36e8dcf56c842150575e7
DESKTOP-RHGLNSB\UIT <21522246@gm.uit.edu.vn>
2022-07-21 21:18:38 +0700

    dev: Login

--- Food_Delivery_App/App.js
+++ Food_Delivery_App/App.js
@@ -1,15 +1,14 @@
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, Image} from 'react-native';
 import React from 'react';
 import CustomButton from './src/components/CustomButton';
+import LoginScreen from './src/screens/auth/Login';
+import {IMG_VEGGIE} from './src/assets/images';
+import {IMG_BACKGROUND} from './src/assets/images';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <CustomButton
-        title={'Login'}
-        alert={"Login button's event handle"}
-        type={'secondary'} //type=[‘primary’, ‘secondary’
-      />
+      <LoginScreen />
     </SafeAreaView>
   );
 };
@@ -17,7 +16,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+  },
+  background: {
+    resizeMode: 'cover',
   },
 });
 
