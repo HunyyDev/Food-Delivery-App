e72cba45fceb9e9bf442b433e65242db1cb34f92
AmosIvor <trantuanvu00000@gmail.com>
2022-07-21 15:19:51 +0700

    Delete Tournesol folder

--- Tournesol/src/constants/color.js
+++
@@ -1,6 +0,0 @@
-const CUSTOM_COLOR = {
-  SunsetOrange: '#FF4B3A',
-  CornflowerBlue: '#6195ED',
-  White: '#FFFFFF',
-};
-export default CUSTOM_COLOR;
--- Tournesol/src/constants/size.js
+++
@@ -1,5 +0,0 @@
-const Size = {
-  Device_Width: 414,
-  Device_Height: 896,
-};
-export default Size;
