60c1b806e51e31fcb079d5e7fd8d6755502d8a25
AmosIvor <trantuanvu00000@gmail.com>
2022-07-21 13:35:46 +0700

    dev: create Login screen

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+const CUSTOM_COLOR = {
+  SunsetOrange: '#FF4B3A',
+  White: '#fff',
+  Red: '#ff0000',
+  Orange: '#FFA500',
+  CornflowerBlue: '#6195ED',
+  Vermilion: '#FA4A0C',
+  Black: '#000',
+  SilverSand : '#C5C4C4',
+  Search: '#EFEEEE',
+  Font_Navigation: '#9A9A9D',
+};
+
+export default CUSTOM_COLOR;
