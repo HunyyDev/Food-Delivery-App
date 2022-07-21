dd2b6df50191d4c7a42e3572438051ea10f3a4fb
DESKTOP-RUGI22K\DELL <quyennguyen18122003@gmail.com>
2022-07-21 21:08:18 +0700

    dev: update font

--- Food_Delivery_App/src/constants/colors.js
+++ Food_Delivery_App/src/constants/colors.js
@@ -1,6 +1,13 @@
 const CUSTOM_COLORS ={
     SunsetOrange: '#FF4B3A',
-    white: 'white',
+    White: '#FFFFFF',
+    Vermilion: '#FF460A',
+    Gallery: '#EFEEEE',
+    Black: '#000000',
+    Silver: '#C5C4C4',
+    Concreate: '#F2F2F2',
+    Manatee: '#9A9A9D',
+
     
 };
 
