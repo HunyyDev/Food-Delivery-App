9691b776afda9d59dbcf8344ddc2a5412130985a
NguyenHuynhPhucLam <@NguyenHuynhPhucLam>
2022-07-22 08:57:02 +0700

    fix: delete icons

--- src/assets/icons/index.js
+++
@@ -1,6 +0,0 @@
-export {default as IC_Bar} from './bar.png';
-export {default as IC_Body} from './body.png';
-export {default as IC_ShoppingCart} from './shopping-cart.png';
-export {default as IC_Head} from './head.png';
-export {default as IC_Timer} from './timer.png';
-export {default as IC_Search} from './visor.png';
