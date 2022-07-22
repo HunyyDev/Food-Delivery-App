734b146b913b2c8c7c73c8d681e196ff89c2f951
DESKTOP-RUGI22K\DELL <quyennguyen18122003@gmail.com>
2022-07-22 08:15:26 +0700

    dev: update

--- Food_Delivery_App/src/components/CustomInput/index.js
+++ Food_Delivery_App/src/components/CustomInput/index.js
@@ -1,53 +1,49 @@
-// import {View, Text, TextInput, StyleSheet} from 'react-native';
-// import React, {useState} from 'react';
-// import scale from '../scale';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import scale from '../scale';
 
-// const CustomInput = props => {
-//   const [value, setValue] = useState();
+const CustomInput = props => {
+  const [value, setValue] = useState();
 
-//   const onChangeText = text => {
-//     setValue(text);
-//   };
+  const onChangeText = text => {
+    setValue(text);
+  };
 
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.containerText}>{props.label}</Text>
-//       {props.secureTextEntry === true ? (
-//         <TextInput
-//           style={styles.containerTextInput}
-//           secureTextEntry
-//           onChangeText={text => onChangeText(text)}
-//         />
-//       ) : (
-//         <TextInput
-//           style={styles.containerTextInput}
-//           onChangeText={text => onChangeText(text)}
-//         />
-//       )}
-//     </View>
-//   );
-// };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.containerText}>{props.label}</Text>
 
-// const styles = StyleSheet.create({
-//   containerText: {
-//     color: '#000',
-//     opacity: 0.4,
-//     fontSize: scale(15),
-//   },
-//   containerTextInput: {
-//     color: '#000',
-//     height: scale(40),
-//     width: scale(314),
-//     fontSize: scale(15),
-//     lineHeight: scale(20),
-//     borderBottomWidth: 1,
-//     borderBottomColor: '#000',
-//   },
-//   container: {
-//     // marginVertical: 25,
-//     marginBottom: scale(24),
-//     alignSelf: 'center',
-//   },
-// });y
+        <TextInput
+          style={styles.containerTextInput}
+          secureTextEntry={props.secureTextEntry}
+          value={value}
+          onChangeText={text => onChangeText(text)}
+        />
+    
+    </View>
+  );
+};
 
-// export default CustomInput;
+const styles = StyleSheet.create({
+  containerText: {
+    color: '#000',
+    opacity: 0.4,
+    fontSize: scale(15),
+  },
+  containerTextInput: {
+    color: '#000',
+    height: scale(40),
+    width: scale(314),
+    fontSize: scale(15),
+    lineHeight: scale(20),
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+  },
+  container: {
+    // marginVertical: 25,
+    marginBottom: scale(24),
+    alignSelf: 'center',
+  },
+});y
+
+export default CustomInput;
