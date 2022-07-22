5446287ea2829c382ef82d9be2b80a1461a60e45
NguyenHuynhPhucLam <@NguyenHuynhPhucLam>
2022-07-22 14:39:45 +0700

    fix: Custom_Text_Input

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import Custom_Text_Input from './src/components/Custom_Text_Input';
 import OnboardingScreen from './src/screens/onboarding';
 
 const App = () => {
-  return <OnboardingScreen></OnboardingScreen>;
+  return <Custom_Text_Input label={'Email'}></Custom_Text_Input>;
 };
 
 export default App;
