1566a8f286268ff5bfa065e7e330f4980134e2be
NguyenHuynhPhucLam <@NguyenHuynhPhucLam>
2022-07-22 08:25:12 +0700

    update: Custom_Text_Input

--- App.js
+++ App.js
@@ -1,32 +1,11 @@
-import {StyleSheet, Text, View, TextInput} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import CUSTOM_COLOR from './src/constants/colors';
-import FONT_FAMILY from './src/constants/fonts';
-const Custom_Text_Input = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  );
+import OnboardingScreen from './src/screens/onboarding';
+
+const App = () => {
+  return <OnboardingScreen></OnboardingScreen>;
 };
 
-export default Custom_Text_Input;
+export default App;
 
-const styles = StyleSheet.create({
-  background: {
-    marginBottom: 45,
-  },
-  title: {
-    color: CUSTOM_COLOR.Black,
-    opacity: 0.4,
-    fontWeight: 'bold',
-  },
-  textInput: {
-    borderBottomColor: CUSTOM_COLOR.Black,
-    borderBottomWidth: 2,
-    color: CUSTOM_COLOR.Black,
-    fontWeight: 'bold',
-    fontSize: 17,
-    fontFamily: FONT_FAMILY.ProTextRegular,
-  },
-});
+const styles = StyleSheet.create({});
