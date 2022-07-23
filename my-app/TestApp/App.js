024d43fa571acb0e7a4e01ecb156a7dfd2296925
@TNB142 <20520142@gm.uit.edu.vn>
2022-07-23 21:55:30 +0700

    code Home Screen

---
+++ my-app/TestApp/App.js
@@ -0,0 +1,14 @@
+import { View, Text,Image, SafeAreaView } from 'react-native'
+import React from 'react'
+import HOME_SCREEN from './src/Screens/HomeScreen'
+
+
+
+
+const App = () => {
+  return (
+  <HOME_SCREEN/>
+  )
+}
+
+export default App
