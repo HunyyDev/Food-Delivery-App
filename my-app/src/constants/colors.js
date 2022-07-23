eb54bee715a0cdd56f02669870ba5f71c491c19b
@TNB142 <20520142@gm.uit.edu.vn>
2022-07-23 21:42:01 +0700

    code Home Screen

---
+++ my-app/src/constants/colors.js
@@ -0,0 +1,11 @@
+const CUSTOM_COLOR ={
+    SunsetOrange: '#FF4B3A',
+    White: '#FFFFFF',
+    CornflowerBlue: '#6195ED',
+    Concrete: '#F2F2F2',
+    Gallery: '#EFEEEE',
+    Black: '#000000',
+    Manatee: "#9A9A9D"
+};
+
+export default CUSTOM_COLOR;
