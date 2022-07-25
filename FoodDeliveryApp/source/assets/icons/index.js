9f80c11e8291f34f458690b765789e7dffe2fb67
PhuocTri0106 <you@PhuocTri0106.com>
2022-07-25 20:44:26 +0700

    dev: Home

--- FoodDeliveryApp/source/assets/icons/index.js
+++ FoodDeliveryApp/source/assets/icons/index.js
@@ -1,7 +1,7 @@
 export {default as IC_Heart} from './IC_Heart';
 export {default as IC_Home} from './IC_Home'
 export {default as IC_Clock} from './IC_Clock';
-export {default as IC_Glass} from './Glass.png';
+export {default as IC_Glass} from './IC_Search';
 export {default as IC_Menu} from './IC_Menu';
 export {default as IC_Cart} from './IC_Cart';
 export {default as IC_User} from './IC_User';
