345c6a1acf9e6bc77afefe202a7debd223c149f6
tiuohs <haolt7102@gmail.com>
2022-07-25 20:50:55 +0700

    commit

--- my-app/src/constants/fonts.js
+++
@@ -1,6 +0,0 @@
-const FONT_FAMILY = {
-  SFProRounded_Heavy: 'SFProRounded-Heavy',
-  SFProText_Heavy: 'SFProText-Heavy',
-  SFProText_Semibold: 'SFProText-Semibold',
-};
-export default FONT_FAMILY;
