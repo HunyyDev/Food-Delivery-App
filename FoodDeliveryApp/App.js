e929d8772dc65ce280346005e3c85806ac7b0ea0
PhuocTri0106 <you@PhuocTri0106.com>
2022-07-25 13:22:45 +0700

    no Message

--- FoodDeliveryApp/App.js
+++ FoodDeliveryApp/App.js
@@ -2,23 +2,22 @@ import React from 'react';
 import LoginScreen from './source/screen/login';
 import Onboarding from './source/screen/onboarding';
 import HomeScreen from './source/screen/home';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Onboarding"
-        screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Onboarding" component={Onboarding} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+        <Stack.Navigator initialRouteName="Onboarding" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Onboarding" component={Onboarding} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
     </NavigationContainer>
   );
 };
   
 export default App;
+  
