8e53d05b3700aa371df98bcacc67137d6454425f
21522611@gm.uit.edu.vn <21522611@gm.uit.edu.vn>
2022-07-25 10:53:08 +0700

    dev

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import OnBoardingScreen from './src/onBoarding/OnBoardingScreen';
 import LoginScreen from './src/auth/LoginScreen';
