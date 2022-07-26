a25187b21a140122248ad349f4aa2d52a4bbabc6
tiuohs <haolt7102@gmail.com>
2022-07-26 15:10:03 +0700

    commit

--- my_app/src/routes/RootNavigator.js
+++ my_app/src/routes/RootNavigator.js
@@ -18,7 +18,7 @@ const RootNavigator = props => {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{headerShown: false}}
-        initialRouteName={SCREEN_NAME.HOME}>
+        initialRouteName={SCREEN_NAME.ProductDetail}>
         <Stack.Screen
           name={SCREEN_NAME.ONBOARDING}
           component={OnboardingScreen}
