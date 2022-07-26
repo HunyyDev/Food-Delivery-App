f848b4ad759c1826722150d237618cba3b521745
tiuohs <haolt7102@gmail.com>
2022-07-26 10:23:19 +0700

    commit

---
+++ my_app/src/constants/custom_font.js
@@ -0,0 +1,15 @@
+const Prefix = 'SFProRounded_';
+
+const CUSTOM_FONTS={
+    Black: Prefix + 'Black',
+    Bold: Prefix + 'Bold',
+    Heavy: Prefix + 'Heavy',
+    Light: `${Prefix}Light`,
+    Medium: `${Prefix}Medium`,
+    Regular: `${Prefix}Regular`,
+    Semibold: `${Prefix}Semibold`,
+    Thin: `${Prefix}Thin`,
+    Ultralight: `${Prefix}Ultralight`,
+};
+
+export default CUSTOM_FONTS;
