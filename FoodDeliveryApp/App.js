a8657975a016265249273cc5ebbe4bc0ee4acc53
DESKTOP-2U35IEK\PC <21521008@gm.uit.edu.vn>
2022-07-27 09:53:12 +0700

    no message

--- FoodDeliveryApp/App.js
+++ FoodDeliveryApp/App.js
@@ -2,22 +2,25 @@ import React from 'react';
 import LoginScreen from './source/screen/login';
 import Onboarding from './source/screen/onboarding';
 import HomeScreen from './source/screen/home';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CartScreen from './source/screen/cart';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Onboarding" screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Onboarding" component={Onboarding} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Cart" component={CartScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-  
+
 export default App;
-  
