a858ab0d76feb77fd38387a790fd4fb04d179f74
@TNB142 <20520142@gm.uit.edu.vn>
2022-07-27 12:35:31 +0700

    bo sung homescreen

--- my_app/src/constants/colors.js
+++ my_app/src/constants/colors.js
@@ -4,5 +4,8 @@ const CUSTOM_COLOR = {
   White: '#FFFFFF',
   Black: '#000000',
   Silver: '#C5C4C4',
+  Concrete: '#F2F2F2',
+  Gallery: '#EFEEEE',
+  Manatee: "#9A9A9D"
 };
 export default CUSTOM_COLOR;
--- my_app/src/constants/fonts.js
+++ my_app/src/constants/fonts.js
@@ -1,6 +1,10 @@
+const Prefix = 'SFProRounded_';
 const FONT_FAMILY = {
   SFProRounded_Heavy: 'SFProRounded-Heavy',
   SFProText_Heavy: 'SFProText-Heavy',
   SFProText_Semibold: 'SFProText-Semibold',
+  SFProText_Regular: `${Prefix}Regular`,
+  SFProText_Bold: Prefix + 'Bold',
+
 };
 export default FONT_FAMILY;
