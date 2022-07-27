1ae972015905d81ed3560c9df0d03634c8074d79
duypham0522 <masterd2205@gmail.com>
2022-07-27 11:34:04 +0700

    dev: src deleted

--- Food_Delivery_App/src/assets/images/index.js
+++
@@ -1,5 +0,0 @@
-export {default as IMG_LOGO} from './logo.png';
-export {default as IMG_BACKGROUND} from './background.png';
-// export {default as IMG_SpicyFishSauce} from './image spicy fish.png';
-export {default as IMG_VeggieTomatoMix} from './VeggieTomatoMix.png';
-export {default as IMG_SpicyFishSauce} from './SpicyFishSauce.png';
