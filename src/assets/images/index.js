e16681622511a5fb4363568db074c55413e398dd
Thịnh Nguyễn Huỳnh Phúc <@21520465>
2022-07-29 10:21:59 +0700

    update screen

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,3 +1,4 @@
 export {default as IMG_Logo} from './logo.png';
 export {default as IMG_Background} from './background.png';
 export {default as IMG_FoodImage} from './food-image.png';
+export {default as IMG_FoodImage2} from './food-image2.png';
--- src/screens/deliveryScreen/index.js
+++ src/screens/deliveryScreen/index.js
@@ -1,11 +1,4 @@
-import {
-  View,
-  Text,
-  Image,
-  TextInput,
-  ScrollView,
-  TouchableOpacity,
-} from 'react-native';
+import {View, Text, Image, ScrollView} from 'react-native';
 import React from 'react';
 import {IC_BackButton} from './src/assets/icons';
 import CustomButton from './src/assets/components/CustomButton';
