08357661ee45c897857f89cfafdbeea980530960
mrtroll2003 <mrbaopirateengland@gmail.com>
2022-07-30 15:39:58 +0700

    update: update loginScreen

--- src/screens/onbroading/indexB.js
+++ src/screens/onbroading/indexB.js
@@ -1,10 +1,12 @@
-import {StyleSheet, Text, View, ScrollView, SafeAreaView, StatusBar, Dimensions, TextInput, useColorScheme, Image, Pressable} from 'react-native';
+import {Text, View, ScrollView, SafeAreaView, StatusBar, TextInput, useColorScheme, Image, Pressable} from 'react-native';
 import React, {useState} from 'react';
 import type {Node} from 'react';
 import {IMG_Logo} from '../../assets/images'
 import CUSTOM_COLOR from '../../constants/colors'
 import FONT_FAMILY from '../../constants/fonts'
 import TAB_NAME from '../../constants/tabName'
+import styles from './styles'
+import UnderlineButton from '../components/UnderlineButton'
 
 function LoginScreen (props)  {
     const {navigation} = props;
@@ -70,15 +72,15 @@ function LoginScreen (props)  {
             </View>
             </>
         )}
-
-    <View style={styles.buttonView}>
+       </ScrollView>
+      <View style={styles.buttonView}>
         <Pressable style={styles.button}>
             <Text style={styles.buttonText}>
                 Login
             </Text>
         </Pressable>
-    </View>
-    </ScrollView>
+      </View>
+
     </SafeAreaView>
   );
 };
@@ -90,105 +92,99 @@ const Holder = () => {
         <Text>{'LoremIpsum'}</Text>
     );
 }
-const UnderlineButton = ({children, isChoosing, onPress}) => {
-  return (
-    <Pressable onPress={onPress} style={styles.navigationBackground({isChoosing})}>
-      <Text style={styles.navigationText}>{children}</Text>
-    </Pressable>
-  );
-};
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor : CUSTOM_COLOR.SilverSand,
-        flex: 1,
-      },
-    logoContainer: {
-          width: '100%',
-          height: '42.63%',
-          backgroundColor: CUSTOM_COLOR.White,
-          overflow: 'hidden',
-          justifyContent: 'center',
-          alignItems: 'center',
-          position: 'absolute',
-          borderBottomLeftRadius: 25,
-          borderBottomRightRadius: 25,
 
-    },
-    logo: {
-        position: 'absolute',
-        width: 131.53,
-        height: 162.38,
-
-    },
-    navigationBar: {
-        position: 'absolute',
-        bottom: 0,
-        width: Dimensions.get('window').width,
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-    },
-
-    navigationBackground: ({isChoosing}) => ({
-        borderBottomWidth: 3,
-        borderBottomColor: isChoosing ? CUSTOM_COLOR.SunsetYellow : CUSTOM_COLOR.Transparent,
-        width: 134,
-    }),
-    navigationText: {
-        color: CUSTOM_COLOR.Black,
-        padding: 15,
-        fontFamily: FONT_FAMILY.ProTextBold,
-        fontSize: 18,
-        alignSelf: 'center',
-    },
-
-    loginContainer: {
-        top: '42.63%',
-    },
-    headlines: {
-        marginTop: 46,
-        fontFamily: FONT_FAMILY.ProTextBold,
-        color: CUSTOM_COLOR.Black,
-        opacity: 0.4,
-    },
-    input: {
-        marginLeft: 50,
-        marginRight: 50,
-        borderBottomWidth: 0.5,
-        borderBottomColor: CUSTOM_COLOR.Black,
-    },
-    inputBox: {
-        color: CUSTOM_COLOR.Black,
-        fontFamily: FONT_FAMILY.ProTextBold,
-    },
-    pressable: {
-        marginTop: 34,
-        marginLeft: 50,
-    },
-    pressableText: {
-        color: CUSTOM_COLOR.OriolesOrange,
-        fontFamily: FONT_FAMILY.ProTextBold,
-    },
-    buttonView: {
-        marginTop: 68,
-        alignItems: 'center',
-    },
-    button: ({pressed}) => ({
-        backgroundColor: CUSTOM_COLOR.SunsetYellow,
-        opacity: pressed ? 0.4 : 1,
-        height: 70,
-        width: 314,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 35,
-        alignSelf: 'center',
-        marginHorizontal: 136,
-        marginBottom: 18,
-    }),
-    buttonText: {
-        color: CUSTOM_COLOR.White,
-        fontFamily: FONT_FAMILY.ProTextBold,
-        fontSize: 18,
-    },
-});
+//const styles = StyleSheet.create({
+//    container: {
+//        backgroundColor : CUSTOM_COLOR.SilverSand,
+//        flex: 1,
+//      },
+//    logoContainer: {
+//          width: '100%',
+//          height: '42.63%',
+//          backgroundColor: CUSTOM_COLOR.White,
+//          overflow: 'hidden',
+//          justifyContent: 'center',
+//          alignItems: 'center',
+//          position: 'absolute',
+//          borderBottomLeftRadius: 25,
+//          borderBottomRightRadius: 25,
+//
+//    },
+//    logo: {
+//        position: 'absolute',
+//        width: 131.53,
+//        height: 162.38,
+//
+//    },
+//    navigationBar: {
+//        position: 'absolute',
+//        bottom: 0,
+//        width: Dimensions.get('window').width,
+//        flex: 1,
+//        flexDirection: 'row',
+//        justifyContent: 'space-evenly',
+//    },
+//
+//    navigationBackground: ({isChoosing}) => ({
+//        borderBottomWidth: 3,
+//        borderBottomColor: isChoosing ? CUSTOM_COLOR.SunsetYellow : CUSTOM_COLOR.Transparent,
+//        width: 134,
+//    }),
+//    navigationText: {
+//        color: CUSTOM_COLOR.Black,
+//        padding: 15,
+//        fontFamily: FONT_FAMILY.ProTextBold,
+//        fontSize: 18,
+//        alignSelf: 'center',
+//    },
+//
+//    loginContainer: {
+//        top: '42.63%',
+//    },
+//    headlines: {
+//        marginTop: 46,
+//        fontFamily: FONT_FAMILY.ProTextBold,
+//        color: CUSTOM_COLOR.Black,
+//        opacity: 0.4,
+//    },
+//    input: {
+//        marginLeft: 50,
+//        marginRight: 50,
+//        borderBottomWidth: 0.5,
+//        borderBottomColor: CUSTOM_COLOR.Black,
+//    },
+//    inputBox: {
+//        color: CUSTOM_COLOR.Black,
+//        fontFamily: FONT_FAMILY.ProTextBold,
+//    },
+//    pressable: {
+//        marginTop: 34,
+//        marginLeft: 50,
+//    },
+//    pressableText: {
+//        color: CUSTOM_COLOR.OriolesOrange,
+//        fontFamily: FONT_FAMILY.ProTextBold,
+//    },
+//    buttonView: {
+//        marginTop: 68,
+//        alignItems: 'center',
+//    },
+//    button: ({pressed}) => ({
+//        backgroundColor: CUSTOM_COLOR.SunsetYellow,
+//        opacity: pressed ? 0.4 : 1,
+//        height: 70,
+//        width: 314,
+//        justifyContent: 'center',
+//        alignItems: 'center',
+//        borderRadius: 35,
+//        alignSelf: 'center',
+//        marginHorizontal: 136,
+//        marginBottom: 18,
+//    }),
+//    buttonText: {
+//        color: CUSTOM_COLOR.White,
+//        fontFamily: FONT_FAMILY.ProTextBold,
+//        fontSize: 18,
+//    },
+//});
