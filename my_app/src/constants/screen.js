7366b037bdf74feb7e2c1399c4144a160ae6a588
tiuohs <haolt7102@gmail.com>
2022-07-30 16:49:44 +0700

    commit

---
+++ my_app/src/constants/screen.js
@@ -0,0 +1,13 @@
+const SCREEN_NAME = {
+  ONBOARDING: 'OnboardingScreen',
+  HOME: 'HomeScreen',
+  LOGIN: 'LoginScreen',
+  SIGNUP: 'SignUpScreen',
+  PRODUCT_LIST: 'ProductListScreen',
+  PRODUCT_DETAIL: 'ProductDetailScreen',
+  SPLASH: 'Splash',
+  PROFILE: 'Profile',
+  MYPROFILE: 'MyProfile',
+};
+
+export default SCREEN_NAME;
