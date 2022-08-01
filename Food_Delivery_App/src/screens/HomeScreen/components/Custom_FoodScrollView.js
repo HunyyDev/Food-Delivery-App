d2a9dc20b7c0ce4c3628c366f967060d18f154af
DESKTOP-RHGLNSB\UIT <21522246@gm.uit.edu.vn>
2022-08-01 09:17:52 +0700

    dev: test test test

--- Food_Delivery_App/src/screens/HomeScreen/components/Custom_FoodScrollView.js
+++ Food_Delivery_App/src/screens/HomeScreen/components/Custom_FoodScrollView.js
@@ -1,6 +1,6 @@
 import {View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import React, {Component} from 'react';
-import { IMG_VEGETABLE } from '../../../assets/images';
+import {IMG_VEGETABLE} from '../../../assets/images';
 import Custom_FoodBoard from './Custom_FoodBoard';
 //import scale from '../../../../responsive';
 import scale from '../../../constants/responsive';
@@ -34,21 +34,28 @@ export default class Custom_FoodScrollView extends Component {
 
   render() {
     return (
-      <ScrollView horizontal={true} style={{...this.props.style}} showsHorizontalScrollIndicator={false}>
+      <ScrollView
+        horizontal={true}
+        style={{...this.props.style}}
+        showsHorizontalScrollIndicator={false}>
         <View style={styles.viewContainer}>
-          {this.state.foodBoard.map(item => (
-            <TouchableOpacity
-             key={item.key} 
-            style={styles.touchContainer} 
-            // onPress = {() => {alert('Tên món ăn: '+item.foodName);}}
-            onPress = {() => {this.props.navigationnn.navigate('FIS')}}  >
-              <Custom_FoodBoard
-                img={item.img}
-                foodName={item.foodName}
-                foodPrice={item.foodPrice}
-              />
-            </TouchableOpacity>
-          ))}
+          {this.state.foodBoard.map(item => {
+            return (
+              <TouchableOpacity
+                key={item.key}
+                style={styles.touchContainer}
+                // onPress = {() => {alert('Tên món ăn: '+item.foodName);}}
+                onPress={() => {
+                  this.props.navigationnn.navigate('FIS');
+                }}>
+                <Custom_FoodBoard
+                  img={item.img}
+                  foodName={item.foodName}
+                  foodPrice={item.foodPrice}
+                />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </ScrollView>
     );
