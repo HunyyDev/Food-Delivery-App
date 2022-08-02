4e8b539466fcdac8310d240b4df7c4fa611f8981
MSI\Admin <21521878@gm.uit.edu.vn>
2022-08-02 23:38:13 +0700

    Merge branch 'WeeBoo/dev' of https://gitlab.com/uit-react-native/food-delivery-app into WeeBoo/dev_BinhNT

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import {Onboarding} from './src/screens/onboarding';
 import {LoginScreen} from './src/screens/login';
 import {WaitingForUpgrade} from './src/screens/WaitingForUpgrade';
@@ -5,6 +6,10 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from './src/screens/HomeScreen';
+import ProductDetail from './src/screens/ProductDetailScreen';
+import CartScreen from './src/screens/Cart';
+import DeliveryScreen from './src/screens/Checkout/DeliveryScreen';
+import PaymentScreen from './src/screens/Checkout/PaymentScreen';
 
 const stack = createNativeStackNavigator();
 
@@ -24,6 +29,19 @@ const App = props => {
         <stack.Screen name="HomeScreen" options={{headerShown: false}}>
           {props => <HomeScreen {...props} />}
         </stack.Screen>
+        <stack.Screen name="ProductDetail" options={{headerShown: false}}>
+          {props => <ProductDetail {...props} />}
+        </stack.Screen>
+        <stack.Screen name="CartScreen" options={{headerShown: false}}>
+          {props => <CartScreen {...props} />}
+        </stack.Screen>
+        <stack.Screen name="DeliveryScreen" options={{headerShown: false}}>
+          {props => <DeliveryScreen {...props} />}
+        </stack.Screen>
+        <stack.Screen name="PaymentScreen" options={{headerShown: false}}>
+          {props => <PaymentScreen {...props} />}
+        </stack.Screen>
+        <stack.Screen name="CustomDrawer" component={'CustomDrawer'} />
       </stack.Navigator>
     </NavigationContainer>
   );
