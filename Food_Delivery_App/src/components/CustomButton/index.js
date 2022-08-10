41311c746da78be7457e93958a98d8100d32170e
DESKTOP-RHGLNSB\UIT <21522246@gm.uit.edu.vn>
2022-08-10 10:55:42 +0700

    update: customButton

--- Food_Delivery_App/src/components/CustomButton/index.js
+++ Food_Delivery_App/src/components/CustomButton/index.js
@@ -1,7 +1,8 @@
 import {useState} from 'react';
-import {StyleSheet, Text, Alert} from 'react-native';
+import {StyleSheet, Text, Alert, Pressable, View} from 'react-native';
 import React from 'react';
 import {scaleX, scaleY} from '../../helperFunction.js';
+import FONT_FAMILY from '../../constants/font.js';
 
 const CustomButton = props => {
   const [buttonColor, setbuttonColor] = useState(() => {
@@ -12,38 +13,51 @@ const CustomButton = props => {
   });
 
   const PressHandler = () => {
+    if (props.name == 'goBack') {
+      props.navigation.navigate.goBack();
+      return;
+    }
     props.navigation.navigate(props.name);
   };
   return (
-    <Text
-      style={[
-        styles.buttonStyle.text,
-        styles.buttonStyle,
-        {backgroundColor: buttonColor.background, color: buttonColor.text},
-      ]}
-      onPress={(PressHandler)}>
-      {props.title}
-    </Text>
+    <View
+      style={[styles.buttonStyle, {backgroundColor: buttonColor.background}]}>
+      <Pressable
+        android_ripple={styles.pressed}
+        style={({pressed}) => (pressed ? styles.pressed : null)}
+        onPress={PressHandler}>
+        <Text style={[styles.text, {color: buttonColor.text}]}>
+          {props.title}
+        </Text>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    padding: 25,
     // backgroundColor: buttonColor.background,
     borderRadius: 70,
-    textAlign: 'center',
-
-    text: {
-      // color: buttonColor.text,
-      fontSize: 17,
-    },
 
     marginHorizontal: scaleY(1),
     position: 'absolute',
     left: 50,
     right: 50,
-    bottom:41,
+    bottom: 41,
+
+    overflow: 'hidden',
+  },
+  pressed: {
+    opacity: 0.75,
+    color: '#ddd',
+    // backgroundColor: '#ddd',
+  },
+  text: {
+    // color: buttonColor.text,
+    padding: 25,
+    textAlign: 'center',
+    fontSize: 17,
+    fontFamily: FONT_FAMILY.Abel,
   },
 });
 
